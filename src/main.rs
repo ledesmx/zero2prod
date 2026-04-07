7faa0b314817a5e84ecef30ef2ba7852ecf0fe51
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2026-04-07 15:06:56 -0600

    change project structure to add testing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use zero2prod::run;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
+    run()
     .await
 }
